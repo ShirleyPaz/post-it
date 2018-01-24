e2738a5519fa4df5a07b95a38b39e3287a479806
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2018-01-24 21:35:53 -0200

    JS finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 notes = [];
 
-function editarFormulario(section,index) {
-// identificar qual a nota que está sendo editada e setar editando = true
-notes[index].editando = true;
-// atualiza tela
-sectionUpdate(section);
-
-}
-
 function sectionUpdate(section) {
 // percorrer cada item da lista de notas, criar o html de cada nota e colocar na variável
 // criar uma variável que vai guardar o HTML de todas as notas que devem aparecer na tela
@@ -19,23 +11,33 @@ for (index=0; index < notes.length; index++) {
     if (notes[index].editando) {
         //template input + textarea
         innerhtml += '<form class="note">'+
-                    '<input class="note__title" type="text" name="title" placeholder="Título" autofocus />'+
-                    '<textarea class="note__body" name="body" rows="5" placeholder="Criar uma nota..."></textarea>'+
-                    '<button class="note__control" type="button" onclick="adicionarNota(this.form,this.form.nextElementSibling,this.form.title,this.form.body)">'+
+                    '<input class="note__title" type="text" name="title" value="'+notes[index].title+'" placeholder="Título"/>'+
+                    '<textarea class="note__body" name="body" rows="5" placeholder="Criar uma nota...">'+notes[index].textarea+'</textarea>'+
+                    '<button class="note__control" type="button" onclick="adicionarNota(this.form,this.form.parentElement,this.form.title,this.form.body,'+index+')">'+
                     'Concluído</button>'+
                 '</form>';
     }
     else {
-        innerhtml += '<form class="note">'+
+        innerhtml += '<form class="note" onclick="editarFormulario(this.parentElement,'+index+')">'+
                     '<button class="note__control" type="button" onClick="removerNota(this.form.parentElement,'+index+')">x</button>'+
-                    '<h1 class="note__title" onclick="editarFormulario(this.form.parentElement,'+index+')">'+notes[index].title+'</h1>'+
-                    '<p class="note__body onclick="editarFormulario(this.form.parentElement,'+index+')">'+notes[index].textarea+'</p>'+
+                    '<h1 class="note__title">'+notes[index].title+'</h1>'+
+                    '<p class="note__body)">'+notes[index].textarea+'</p>'+
                 '</form>';     
     }
 }
 
 section.innerHTML = innerhtml;
 
+}
+
+function editarFormulario(section,index) {
+// identificar qual a nota que está sendo editada e setar editando = true
+if (index != null) {
+notes[index].editando = true;
+// atualiza tela
+sectionUpdate(section);
+
+}
 }
 
 function removerNota(section,index) {
@@ -46,7 +48,19 @@ notes.splice(index,1);
 sectionUpdate(section);
 }
 
-function adicionarNota(form,section,title,textarea) {
+function adicionarNota(form,section,title,textarea,index) {
+// verificiar se a nota está sendo editada
+if (index != null) { // ou if (notes[index]))
+    // identificar a nota sendo editada
+     //trocar título e o texto dela
+    notes[index].title = title.value;
+    notes[index].textarea = textarea.value;
+    notes[index].editando = false;
+   
+    //chamar atualiza seção
+    sectionUpdate(section);
+
+} else {
 // criar um objeto nota
 var note ={
     title: title.value,
@@ -62,4 +76,5 @@ sectionUpdate(section);
 
 //limpar o formulário
 form.reset();
-} 
+}
+}
